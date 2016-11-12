43ae4bc91ea094719e2ed64223d3c36b7e686cd2
KelvinFranco <kelvinfranco96@hotmail.com>
2016-11-12 07:50:20 -0600

    Modificacion
    Se modifico el mensaje

--- src/demooperaciones/frmOperaciones.java
+++ src/demooperaciones/frmOperaciones.java
@@ -101,7 +101,7 @@ public class frmOperaciones extends javax.swing.JFrame {
         // TODO add your handling code here:
         int numA = Integer.parseInt(txtNumA.getText());
         int numB = Integer.parseInt(txtNumB.getText());
-        JOptionPane.showMessageDialog(this, "La suma es: " + (numA + numB));
+        JOptionPane.showMessageDialog(this, "La sumatoria de los numeros es: " + (numA + numB));
     }//GEN-LAST:event_btnSumarActionPerformed
 
     /**
